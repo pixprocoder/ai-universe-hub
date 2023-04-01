3861a696134bee1317daab1cd168ef2698734ef2
skpixels <makkobir79@gmail.com>
2023-04-01 13:09:31 +0600

    all tools load api code written

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("connected");
+const loadAllData = async () => {
+  const url = "https://openapi.programming-hero.com/api/ai/tools";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayData(data.data.tools);
+};
+
+const displayData = (tools) => {
+  for (const tool of tools) {
+    console.log(tool);
+  }
+};
+
+loadAllData();
