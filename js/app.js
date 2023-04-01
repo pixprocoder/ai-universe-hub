a0e6da270a4e304032085290476fcb5126e8b692
skpixels <makkobir79@gmail.com>
2023-04-01 16:29:03 +0600

    daisy ui cdn added

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ const loadAllData = async () => {
 };
 
 const displayData = (tools) => {
+  const showAll = document.getElementById("show-all-btn");
+
+  if (tools.length >= 6) {
+    tools = tools.slice(0, 6);
+    showAll.classList.remove("hidden");
+  } else {
+    // showAll.classList.add("block");
+  }
+
   tools.forEach((tool) => {
     const cardContainer = document.getElementById("card-container");
     const cardDiv = document.createElement("div");
@@ -32,11 +41,11 @@ const displayData = (tools) => {
         <p class="my-2">${tool.published_in}</p>
         </div>
        <div>
-       <button class="px-3 py-1 text-lg text-white font-bold bg-orange-400 rounded-md">Details</button>
+       <label for="my-modal-3" class="btn px-3 py-1 text-lg text-white font-bold bg-orange-400 rounded-md">Details</label>
        </div>
     </div>
     `;
-    console.log(tool);
+    // console.log(tool);
     cardContainer.appendChild(cardDiv);
   });
 };
